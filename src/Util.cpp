4f63a2060bd865f0c0cf55812b128427701b724d
jared <j-wasinger@hotmail.com>
2014-07-11 12:42:32 -0700

    .

--- src/Util.cpp
+++ src/Util.cpp
@@ -3,14 +3,13 @@
 #include <SDL2/SDL.h>
 #include <cstdio>
 
-bool checkSDLError(void);
+bool checkSDLError(void)
 {
     const char *error = SDL_GetError();
     if (*error != '\0')
     {
         printf("SDL Error: %s\n", error);
-        if (line != -1)
-            printf(" + line: %i\n", line);
+        printf(" + line: %i\n", __LINE__);
         SDL_ClearError();
         return true;
     }
